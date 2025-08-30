b245c2a4bdddbf22490b45c5e19058b4a927f791
H6rst0n <104990977+H6rst0n@users.noreply.github.com>
2025-08-31 05:23:09 +0800

    只保留要用的

--- gpush.sh
+++
@@ -1,9 +0,0 @@
-git pull origin master
-npm install
-git  add .
-git commit  -m  "$*"
-git remote add origin https://github.com/BigBroSci-LVTHW/LVTHW.git
-git remote -v
-git push origin  master
-
-hexo generate && hexo deploy
--- source/tags/index.md
+++
@@ -1,6 +0,0 @@
----
-title: 标签
-date: 2018-10-21 21:40:58
-type: "tags"
-layout: "tags"
----
